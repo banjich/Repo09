ee84d19be466564f66df133b356c06a16d61c287
banjich <banjich@gmail.com>
2021-09-22 15:29:45 +0200

    Added file index.js

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -2,19 +2,15 @@ function myFunction(myParameter) {
 
     if (myParameter) {
     return "The parameter is true!"
-
     }
     return "The parameter is false!"
-    }
-
-        
-
-
+}
 
 myFunction(true);
 myFunction(false);
 
+
 console.log(myFunction(true));
 console.log(myFunction(false));
 
-module.exports - myFunction;
+module.exports = myFunction;
